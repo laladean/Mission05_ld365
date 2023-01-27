b89466759cd873a39636af4c4efc86abec3d3262
laladean <laurendeano@gmail.com>
2023-01-26 19:10:27 -0700

    Add project files.

---
+++ mission4/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,22 @@
+﻿$("#submit-btn").click(function () {
+
+    const A = .5;
+    const P = .1;
+    const Q = .1;
+    const M = .1;
+    const F = .1;
+    const I = .1;
+
+    var assignment = $("#assign").val();
+    var project = $("#proj").val();
+    var quizzes = $("#quiz").val();
+    var midterm = $("#mid").val();
+    var final = $("#final").val();
+    var intex = $("#intex").val();
+
+    var finalGrade = (assignment * A) + (project * P) + (quizzes * Q) + (midterm * M) + (final * F) + (intex * I);
+
+    var message = "Your Final Grade is: " + finalGrade.toString() + "%";
+    alert(message);
+
+})
